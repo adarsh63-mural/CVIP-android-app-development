c4b86b983f65d02f1c5bd0b882781534ee1876c7
adarsh63-mural <147500120+adarsh63-mural@users.noreply.github.com>
2023-11-15 14:04:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const taskInput = document.getElementById("task-input");
-    const addTaskButton = document.getElementById("add-task");
-    const taskList = document.getElementById("task-list");
+let currentPlayer = 'X';
+let gameBoard = Array(9).fill('');
 
-    addTaskButton.addEventListener("click", function () {
-        const taskText = taskInput.value;
-        if (taskText.trim() !== "") {
-            createTask(taskText);
-            taskInput.value = "";
-        }
-    });
+function makeMove(cell) {
+    const index = Array.from(cell.parentElement.children).indexOf(cell);
 
-    function createTask(text) {
-        const taskItem = document.createElement("li");
-        taskItem.classList.add("task-item");
-        taskItem.innerHTML = `
-            <input type="text" value="${text}">
-            <button class="update-task">Update</button>
-            <button class="delete-task">Delete</button>
-        `;
-        taskList.appendChild(taskItem);
-
-        const deleteButton = taskItem.querySelector(".delete-task");
-        deleteButton.addEventListener("click", function () {
-            taskList.removeChild(taskItem);
-        });
-
-        const updateButton = taskItem.querySelector(".update-task");
-        updateButton.addEventListener("click", function () {
-            const newTaskText = taskItem.querySelector("input").value;
-            if (newTaskText.trim() !== "") {
-                taskItem.querySelector("input").value = newTaskText;
-            }
-        });
+    if (gameBoard[index] === '' && !checkWinner()) {
+        gameBoard[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        document.getElementById('message').textContent = `Player ${currentPlayer}'s turn`;
+        checkWinner();
     }
-});
+}
+
+function checkWinner() {
+    const winCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (const combination of winCombinations) {
+        const [a, b, c] = combination;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            document.getElementById('message').textContent = `Player ${gameBoard[a]} wins!`;
+            return true;
+        }
+    }
+
+    if (gameBoard.every(cell => cell !== '')) {
+        document.getElementById('message').textContent = 'It\'s a draw!';
+        return true;
+    }
+
+    return false;
+}
+
+function resetGame() {
+    gameBoard = Array(9).fill('');
+    currentPlayer = 'X';
+    document.getElementById('message').textContent = 'Player X\'s turn';
+    const cells = document.getElementsByClassName('cell');
+    for (const cell of cells) {
+        cell.textContent = '';
+    }
+}
+
+resetGame();  // Initialize the game
+
